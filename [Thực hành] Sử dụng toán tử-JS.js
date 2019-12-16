4b14945a90c2a2302c8173a98e4778e86ada83f7
hoangnguyen294 <hoangnguyen294.qa@gmail.com>
2019-12-16 14:09:22 +0700

    baitap

---
+++ [Thực hành] Sử dụng toán tử-JS.js
@@ -0,0 +1,6 @@
+let inputwidht = prompt("Nhập chiều rộng");
+let inputheight = prompt("Nhập chiều cao");
+let widht = parseInt(inputwidht);
+let height = parseInt(inputheight);
+let area = widht * height;
+document.write("The area is : " + area);
